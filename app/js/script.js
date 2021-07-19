524fd47139b08afe7b47cbae08c4b0ea4545f37e
cristhian vargas <cristhian.a.vargas@gmail.com>
2021-07-18 20:17:54 -0500

    add 20px to top positioning on the links for the mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -16,7 +16,7 @@ const navSlide = () => {
         link.style.animation = "";
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.2
+          index / 5 
         }s`;
       }
     });
