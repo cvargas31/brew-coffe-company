b919343ba9cafd9ea8d4a15f37f886676068e57b
cristhian vargas <cristhian.a.vargas@gmail.com>
2021-07-18 19:48:27 -0500

    added mobile menu

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+  const burger = document.querySelector(".header__mobile__burger");
+  const nav = document.querySelector(".header__mobile__nav-links");
+  const navLinks = document.querySelectorAll(
+    ".header__mobile__nav-links__container li"
+  );
+
+  burger.addEventListener("click", () => {
+    //toggle Nav
+    nav.classList.toggle("nav-active");
+
+
+    // animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.2
+        }s`;
+      }
+    });
+  });
+
+};
+
+navSlide();
